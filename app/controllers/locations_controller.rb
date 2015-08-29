90ae2b49578eafc496f8787274102ff9fbcce3eb
Rachael Kalicun <rachael@kalicun.com>
2015-08-29 14:49:36 -0600

    initial setup of geocoding/gmaps

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,6 +5,10 @@ class LocationsController < ApplicationController
   # GET /locations.json
   def index
     @locations = Location.all
+    @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
+      marker.lat location.latitude
+      marker.lng location.longitude
+    end
   end
 
   # GET /locations/1
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,7 @@
 class Location < ActiveRecord::Base
+
+  geocoded_by :address
+  after_validation :geocode
+
+
 end
